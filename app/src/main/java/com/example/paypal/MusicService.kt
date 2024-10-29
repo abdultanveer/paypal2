321674bf295589f84915bdfd4a063d686d85babe
abdul <abdul.tanveer@gmail.com>
2024-10-29 12:17:55 +0530

    playing a music file

--- app/src/main/java/com/example/paypal/MusicService.kt
+++ app/src/main/java/com/example/paypal/MusicService.kt
@@ -2,6 +2,7 @@ package com.example.paypal
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 import android.util.Log
 import androidx.core.util.LogWriter
@@ -17,6 +18,9 @@ var TAG = MusicService::class.java.simpleName
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
          super.onStartCommand(intent, flags, startId)
         var data = intent?.getStringExtra("co")
+        //context = history
+        var winamp = MediaPlayer.create(this,R.raw.music)
+        winamp.start()
         Log.d(TAG,"onstart--"+data)
 
         return START_STICKY
