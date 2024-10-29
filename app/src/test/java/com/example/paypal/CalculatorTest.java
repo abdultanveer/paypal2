b0dc8944afa58b125ee65a8bea7fc21097e6700e
abdul <abdul.tanveer@gmail.com>
2024-10-29 14:35:24 +0530

    unit testing

---
+++ app/src/test/java/com/example/paypal/CalculatorTest.java
@@ -0,0 +1,22 @@
+package com.example.paypal;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest extends TestCase {
+Calculator calculator;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        calculator = new Calculator();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testAdd() {
+        int expected = 40;
+        int actual = calculator.add(10,20);
+
+        assertEquals(expected,actual);
+    }
+}
