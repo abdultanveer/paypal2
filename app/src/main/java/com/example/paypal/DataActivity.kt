a0c74b5308b66255c04d45c2f33b4e34150e1129
abdul <abdul.tanveer@gmail.com>
2024-10-30 10:39:51 +0530

    datastorage2- insert

--- app/src/main/java/com/example/paypal/DataActivity.kt
+++ app/src/main/java/com/example/paypal/DataActivity.kt
@@ -1,20 +1,33 @@
 package com.example.paypal
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.paypal.database.Item
+import com.example.paypal.database.ItemDao
+import com.example.paypal.database.ItemRoomDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class DataActivity : AppCompatActivity() {
+
+    lateinit var dao: ItemDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_data)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        var database = ItemRoomDatabase.getDatabase(this)
+        dao = database.itemDao()
+
+    }
+
+    fun commitData(view: View) {
+        var gItem = Item(11, "fruits", 12.99, 12)
+        GlobalScope.launch {
+            dao.insert(gItem)
         }
     }
 }
